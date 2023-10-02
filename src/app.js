3dce60028ef40a237aef169c65fe0df1c80194ec
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2023-10-02 19:00:54 +0000

    html y js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
+
+// Obtén el elemento HTML por su id
+const excuseElement = document.getElementById("excuse");
+
+// Genera la excusa y actualiza el contenido del elemento
+const randomExcuse = generateExcuse();
+excuseElement.textContent = randomExcuse;
